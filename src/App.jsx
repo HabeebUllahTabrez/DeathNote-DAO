454130210db6930fcf3184cf0b1cf87a65920149
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2022-04-08 00:53:36 +0530

    changed dao name

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to StudentDAO!</h1>
+        <h1>Welcome to DeathNote DAO!</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>

bcdd977a54c6e8e53df69ecd66f1fe25b7bde4d2
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2022-04-08 01:45:14 +0530

    added screens and minting logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,95 @@
-import { useAddress, useMetamask } from '@thirdweb-dev/react';
-import React from 'react';
+import { useAddress, useMetamask, useEditionDrop } from "@thirdweb-dev/react";
+import { useState, useEffect, React } from "react";
 
 const App = () => {
-  // Use the hooks thirdweb give us.
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  console.log("👋 Address:", address);
+    // Use the hooks thirdweb give us.
+    const address = useAddress();
+    const connectWithMetamask = useMetamask();
+    console.log("👋 Address:", address);
 
-  // This is the case where the user hasn't connected their wallet
-  // to your web app. Let them call connectWallet.
-  if (!address) {
-    return (
-      <div className="landing">
-        <h1>Welcome to DeathNote DAO!</h1>
-        <button onClick={connectWithMetamask} className="btn-hero">
-          Connect your wallet
-        </button>
-      </div>
+    // Initialize our editionDrop contract
+    const editionDrop = useEditionDrop(
+        "0xDD0caFEd1584AD792c8E719489BB94E2Ce6D1d91"
     );
-  }
+    // State variable for us to know if user has our NFT.
+    const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+    // isClaiming lets us easily keep a loading state while the NFT is minting.
+    const [isClaiming, setIsClaiming] = useState(false);
 
-  // This is the case where we have the user's address
-  // which means they've connected their wallet to our site!
-  return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
-    </div>);
-}
+    useEffect(() => {
+        // If they don't have an connected wallet, exit!
+        if (!address) {
+            return;
+        }
+
+        const checkBalance = async () => {
+            try {
+                const balance = await editionDrop.balanceOf(address, 0);
+                if (balance.gt(0)) {
+                    setHasClaimedNFT(true);
+                    console.log("🌟 this user has a membership NFT!");
+                } else {
+                    setHasClaimedNFT(false);
+                    console.log("😭 this user doesn't have a membership NFT.");
+                }
+            } catch (error) {
+                setHasClaimedNFT(false);
+                console.error("Failed to get balance", error);
+            }
+        };
+
+        checkBalance();
+    }, [address, editionDrop]);
+
+    const mintNft = async () => {
+        try {
+            setIsClaiming(true);
+            await editionDrop.claim("0", 1);
+            console.log(
+                `🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`
+            );
+            // https://testnets.opensea.io/assets/0xDD0caFEd1584AD792c8E719489BB94E2Ce6D1d91/0
+            setHasClaimedNFT(true);
+        } catch (error) {
+            setHasClaimedNFT(false);
+            console.error("Failed to mint NFT", error);
+        } finally {
+            setIsClaiming(false);
+        }
+    };
+
+    // This is the case where the user hasn't connected their wallet
+    // to your web app. Let them call connectWallet.
+    if (!address) {
+        return (
+            <div className="landing">
+                <h1>Welcome to DeathNote DAO!</h1>
+                <button onClick={connectWithMetamask} className="btn-hero">
+                    Connect your wallet
+                </button>
+            </div>
+        );
+    }
+
+    // Add this little piece!
+    if (hasClaimedNFT) {
+        return (
+            <div className="member-page">
+                <h1>📓DeathNote DAO Member Page</h1>
+                <p>Congratulations on being a member!</p>
+            </div>
+        );
+    }
+
+    // Render mint nft screen.
+    return (
+        <div className="mint-nft">
+            <h1>Mint your free 📓 DAO Membership NFT</h1>
+            <button disabled={isClaiming} onClick={mintNft}>
+                {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+            </button>
+        </div>
+    );
+};
 
 export default App;
